36ffad3fb22b44f22c7f2deb1cac0028c605d220
Harshit Buwade <harshitpawar04@gmail.com>
2026-05-20 17:18:18 +0530

    Bubble sort on list

---
+++ Practice Code/sorting.py
@@ -0,0 +1,19 @@
+#Bubble sort
+list1 = [1,2,3,4,5]  #[8, 10, 6, 2, 4]
+swapped = True
+count = 0
+while swapped:
+    swapped = False
+    for i in range(len(list1)-1):
+        count += 1
+        if list1[i] > list1[i+1]:
+            swapped = True
+            list1[i], list1[i+1] = list1[i+1], list1[i]
+print(list1)
+print(count)
+
+
+#Sort Method
+list = [8,3,2,9]
+list.sort()
+print(list)
