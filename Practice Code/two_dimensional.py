d5e96f8f363cb1be97597a6a0f734bb1e641aab9
Harshit Buwade <harshitpawar04@gmail.com>
2026-05-21 17:10:10 +0530

    chess

--- Practice Code/two_dimensional.py
+++ Practice Code/two_dimensional.py
@@ -5,3 +5,33 @@ for i in range(8):
 #print(board)
 for index in board:
     print(index)
+# print(len(board))
+# print(board[0][0])
+board[0][0] = "ROOKS"
+board[0][7] = "ROOKS"
+board[7][0] = "ROOKS"
+board[7][7] = "ROOKS"
+
+board[0][1] = "KNIGHT"
+board[0][6] = "KNIGHT"
+board[7][1] = "KNIGHT"
+board[7][6] = "KNIGHT"
+
+board[0][2] = "BISHOP"
+board[0][5] = "BISHOP"
+board[7][2] = "BISHOP"
+board[7][5] = "BISHOP"
+
+board[0][3] = "QUEEN"
+board[0][4] = "KING"
+board[7][3] = "QUEEN"
+board[7][4] = "KING"
+
+board[1] = ["PAWN" for i in range(8)]
+board[6] = ["PAWN" for i in range(8)]
+
+
+print("**********")
+for index in board:
+    print(index)
+
