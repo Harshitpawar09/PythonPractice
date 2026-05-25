d290c5ba566d64ff5751be59fab4d4f76fee06fd
Harshit Buwade <harshitpawar04@gmail.com>
2026-05-25 16:23:38 +0530

    practice code

--- Practice Code/practice.py
+++ Practice Code/practice.py
@@ -94,9 +94,161 @@
 
 
 
-numbers = [1, 2, 3, 4, 5]
-print(len(numbers))
-del numbers[-1]
-print(numbers)
-numbers[len(numbers)//2] = int(input("Enter a number: "))
-print(numbers)  
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers))
+# del numbers[-1]
+# print(numbers)
+# numbers[len(numbers)//2] = int(input("Enter a number: "))
+# print(numbers)  
+
+
+#Practice code
+# list1 = [1, 2, 3, 4, 5]
+# print(len(list1))
+# del list1[-1]
+# print(list1)
+# list1[len(list1)//2] = int(input("Enter a number: "))
+# print(list1)
+
+#2nd
+# beatles = []
+# beatles.append("John Lennon")
+# beatles.append("Paul McCartney")
+# beatles.append("George Harrison")
+# for i in range(2):
+#     member = input("Add member: ")
+#     beatles.append(member)
+# del beatles[-2]
+# del beatles[-1]
+# beatles.insert(0, "Ringo Starr")
+# print(beatles)
+
+#3rd
+#a
+# result = []
+
+# for i in range(1, 51):
+#     if i % 3 == 0 and i % 5 == 0:
+#         result.append("FizBuzz")
+#     elif i % 3 == 0:
+#         result.append("Fiz")
+#     elif i % 5 == 0:
+#         result.append("Buzz")
+#     else:
+#         result.append(i)
+
+# print(result)
+
+
+#b
+# string = "MindCoders password2 is : 1234"
+
+# count = 0
+
+# for i in string:
+#     if i.isdigit():
+#         count += 1
+
+# print("Total number of Digits =", count)
+
+#c
+# string = "U r a a n S 0 f t s k i l l 1 s 1234"
+
+# count = 0
+
+# for i in string:
+#     if i.isdigit():
+#         count += 1
+
+# print("Total number of Digits =", count)
+
+#d
+# string = "MindCoders"
+
+# count = 0
+
+# for i in string:
+#     if i == 's' or i == 'S':
+#         count += 1
+
+# print(count)
+
+
+# #5th
+# # 1. First 10 natural numbers
+# print("1.")
+# for i in range(1, 11):
+#     print(i)
+
+# # 2. Even numbers within range 10
+# print("\n2.")
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print(i)
+
+# # 3. Sum from 1 to 15
+# print("\n3.")
+# sum = 0
+# for i in range(1, 16):
+#     sum += i
+# print(sum)
+
+# # 4. Sum of odd numbers within range 15
+# print("\n4.")
+# odd_sum = 0
+# for i in range(1, 16):
+#     if i % 2 != 0:
+#         odd_sum += i
+# print(odd_sum)
+
+# # 5. Multiplication table of 15
+# print("\n5.")
+# for i in range(1, 11):
+#     print("15 x", i, "=", 15 * i)
+
+# # 6. Display numbers from list
+# print("\n6.")
+# numbers = [1, 2, 4, 6, 88, 125]
+# for i in numbers:
+#     print(i)
+
+# # 7. Count digits in number
+# print("\n7.")
+# num = 129475
+# count = 0
+# while num > 0:
+#     count += 1
+#     num //= 10
+# print(count)
+
+# # 8. Palindrome check
+# print("\n8.")
+# text = "madam"
+# if text == text[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# # 9. Reverse a word
+# print("\n9.")
+# word = input("Enter word: ")
+# print(word[::-1])
+
+# # 10. Armstrong number check
+# print("\n10.")
+# num = 153
+# temp = num
+# total = 0
+
+# while temp > 0:
+#     digit = temp % 10
+#     total += digit ** 3
+#     temp //= 10
+
+# if total == num:
+#     print("Armstrong Number")
+# else:
+#     print("Not Armstrong Number")
+
+#4th
+
--- Practice Code/tempCodeRunnerFile.py
+++ Practice Code/tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-print (new_list)
+
+# #d
+# string = "MindCoders"
+
+# count = 0
+
+# for i in string:
+#     if i == 's' or i == 'S':
+#         count += 1
+
+# print(count)
