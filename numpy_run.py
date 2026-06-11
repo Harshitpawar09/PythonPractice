75b2badc38d134fcc4ad3a2de5423d5794057ac6
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-11 18:04:28 +0530

    Numpy

---
+++ numpy_run.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Create Arrays
+arr1d = np.array([1, 2, 3, 4, 5])
+arr2d = np.array([[34, 65, 24], [53, 87, 50], [29, 36, 95]])
+
+print(arr2d.shape)
+print(arr2d.dtype)
+print(arr2d.ndim)
