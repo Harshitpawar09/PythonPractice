1b3812f5a69f27f2f3960f611ad9dfdd938b2377
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-11 17:18:41 +0530

    CLI CSV Practice

--- filehandling.py
+++ filehandling.py
@@ -27,13 +27,42 @@
 
 
 
+# import csv
+# #writing csv
+# records = [
+#     ['Name', 'Marks', 'City', 'Grade'],
+#     ['Alice', 85, 'New York', 'A'],
+#     ['Bob', 90, 'Los Angeles', 'A+'],
+#     ['Charlie', 78, 'Chicago', 'B']
+# ]
+# with open('students.csv', 'w', newline='') as file:
+#     csv.writer(file).writerows(records)
+
+
+# #reading csv
+# with open('students.csv', 'r') as file:
+#     for row in csv.DictReader(file):
+#         print(f'{row["Name"]}: {row["Marks"]} marks ({row["City"]})')
+
+
 import csv
-#writing csv
 records = [
-    ['Name', 'Marks', ' city', 'Grade'],
-    ['Alice', 85, 'New York', 'A'],
-    ['Bob', 90, 'Los Angeles', 'A+'],
-    ['Charlie', 78, 'Chicago', 'B']
+    ['Name', 'Age', 'Marks1', 'Marks2', 'Marks3'],
+    ['Harshit', 22, 88, 89, 93],
+    ['Divya', 22, 88, 89, 93],
+    ['Abhishek', 22, 88, 89, 93],
+    ['Harsh', 22, 88, 89, 93]
 ]
-with open('students.csv', 'w', newline='') as file:
+with open('CLI.csv', 'w', newline='') as file:
     csv.writer(file).writerows(records)
+
+student = input("Enter Student Name:")
+found = False
+with open('CLI.csv', 'r', newline='') as file:
+    for row in csv.DictReader(file):
+        if row['Name'] == student:
+            print(f"{row['Name']}: age {row['Age']} marks1 {row['Marks1']} marks2 {row['Marks2']} marks3 {row['Marks3']}")
+            found = True
+            break
+if not found:
+        print("No Student Record Found")
