826387258664292702fc29284e65d1d4fe8d83f2
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-11 16:22:24 +0530

    csv read write

--- filehandling.py
+++ filehandling.py
@@ -4,18 +4,36 @@
 
 
 
-with open("students.txt", "w") as file:
-    file.write("Alice, 85\n")
-    file.write("Bob, 90\n")
-    file.write("Charlie, 78\n")
+# with open("students.txt", "w") as file:
+#     file.write("Alice, 85\n")
+#     file.write("Bob, 90\n")
+#     file.write("Charlie, 78\n")
 
-with open("students.txt", "r") as file:
-    content = file.read()
-print(content)
+# with open("students.txt", "r") as file:
+#     content = file.read()
+# print(content)
 
 
-with open("students.txt", "r") as file:
-    for line in file:
-        name, score = line.strip().split(", ")
-        print(f"{name:<15} |{score:>5}")
-        print("----------")
+# with open("students.txt", "r") as file:
+#     for line in file:
+#         name, score = line.strip().split(", ")
+#         print(f"{name:<15} |{score:>5}")
+#         print("----------")
+
+
+
+
+#CSV 
+
+
+
+import csv
+#writing csv
+records = [
+    ['Name', 'Marks', ' city', 'Grade'],
+    ['Alice', 85, 'New York', 'A'],
+    ['Bob', 90, 'Los Angeles', 'A+'],
+    ['Charlie', 78, 'Chicago', 'B']
+]
+with open('students.csv', 'w', newline='') as file:
+    csv.writer(file).writerows(records)
