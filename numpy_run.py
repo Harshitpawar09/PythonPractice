559cf7fc9ad5f45804fc347a0d115cc371dceacc
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-12 16:55:09 +0530

    Numpy completed

--- numpy_run.py
+++ numpy_run.py
@@ -1,9 +1,29 @@
 import numpy as np
 
-# Create Arrays
-arr1d = np.array([1, 2, 3, 4, 5])
-arr2d = np.array([[34, 65, 24], [53, 87, 50], [29, 36, 95]])
+# # Create Arrays
+# arr1d = np.array([1, 2, 3, 4, 5])
+# arr2d = np.array([[34, 65, 24], [53, 87, 50], [29, 36, 95]])
 
-print(arr2d.shape)
-print(arr2d.dtype)
-print(arr2d.ndim)
+# print(arr2d.shape)
+# print(arr2d.dtype)
+# print(arr2d.ndim)
+
+
+
+# zeros = np.zeros((3,4))
+# print(zeros)
+# ones = np.ones((2,5))
+# print(ones)
+# rng = np.arange(0,50,5)
+# print(rng)
+# lin = np.linspace(0,1,11)
+# print(lin)
+# random = np.random.randint(40,100,(5,3))
+# print(random)
+
+
+
+arr = np.array([10,20,30,40,50])
+print(arr * 2)
+print(arr + 5)
+print(arr ** 2)
