ebe40d9b45f2dc19f3fef59302d82e547e8141b3
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-15 17:05:30 +0530

    Pandas

--- pandassss.py
+++ pandassss.py
@@ -11,3 +11,22 @@ print(df.shape)
 print(df.head(3))
 print(df.dtypes)
 print(df.describe)
+
+#select column
+print("df['Name']: \n", df['Name'])
+print(df[['Name', 'Marks']])
+
+#Filter Row
+print(df[df['Marks'] >= 85])
+print(df[df['City'] == 'Bhopal'])
+print(df[(df['Marks'] >=80) & (df['City'] == 'Indore' )])
+def get_grade(x):
+    if x >= 90:
+        return 'A'
+    elif x >= 75:
+        return 'B'
+    else:
+        return 'C'
+df['Grade'] = df['Marks'].apply(get_grade)
+print(df['Grade'])
+print(df)
