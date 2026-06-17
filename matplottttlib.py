76014fbf74c1b1e9672066e0011719b8dd9ecd6f
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-17 17:55:25 +0530

    ml

--- matplottttlib.py
+++ matplottttlib.py
@@ -44,12 +44,12 @@ import numpy as np
 
 import seaborn as sns
 import pandas as pd
-np.random.seed(42)
-df = pd.DataFrame({
-    'marks': np.random.randint(40,100,100),
-    'study_hours': np.random.uniform(2,10,100),
-    'city': np.random.choice(['Bhopal', 'Indore', 'Jabalpur'],100),
-    'gender': np.random.choice(['male','Female'],100)})
+# np.random.seed(42)
+# df = pd.DataFrame({
+#     'marks': np.random.randint(40,100,100),
+#     'study_hours': np.random.uniform(2,10,100),
+#     'city': np.random.choice(['Bhopal', 'Indore', 'Jabalpur'],100),
+#     'gender': np.random.choice(['male','Female'],100)})
 # plt.figure(figsize=(10,4))
 # sns.histplot(df['marks'], bins = 20, kde =True, color='steelblue')
 # plt.title('Distribution of student marks')
