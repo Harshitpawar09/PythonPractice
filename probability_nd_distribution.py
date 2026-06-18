4173b6d4c80af1123934308e6023e0b356a565ca
Harshit Buwade <harshitpawar04@gmail.com>
2026-06-18 17:43:54 +0530

    probability

---
+++ probability_nd_distribution.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
+
+mean_h, std_h = 165, 7
+prob = 1 - norm.cdf(175, mean_h, std_h)
+print(f'P(height > 175 cm) = {prob:.4f} = {prob*100:.1f}%')
+print(f'68% of people: {mean_h-std_h:.0f}cm to {mean_h+std_h:.0f}cm')
+print(f'95% of people: {mean_h-2*std_h:.0f}cm to {mean_h+2*std_h:.0f}cm')
+print(f'99.7% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h:.0f}cm')
